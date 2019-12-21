f4ad274fb1b05368dec8641573f7601eefe952b9
Yooooomi <timothee.boussus@gmail.com>
2019-12-21 20:46:25 +0100

    Fixed start scripts with port replacements

--- client/run.sh
+++ client/run.sh
@@ -4,6 +4,7 @@ if [[ -z "$WEB_PORT" ]]
 then
     echo "No WEB_PORT provided, using default 3000"
     export WEB_PORT=3000
+    WEB_PORT=3000
 fi
 
 serve -l "0.0.0.0:$WEB_PORT" build/
--- server/run.sh
+++ server/run.sh
@@ -4,6 +4,7 @@ if [[ -z "$SERVER_PORT" ]]
 then
     echo "No WEB_PORT provided, using default 8081"
     export SERVER_PORT=8081
+    SERVER_PORT=8081
 fi
 
 npm start
