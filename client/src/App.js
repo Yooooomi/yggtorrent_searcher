a6a8229308f20811f70d027b03833bb54623a049
Yooooomi <timothee.boussus@gmail.com>
2019-12-22 13:12:49 +0100

    Added possibility to use the service on localnetwork

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './App.css';
 import Main from './scenes/Main';
+import API from './services/API';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Torrent searcher
-      </header>
-      <div className="App-content">
-        <Main />
+class App extends React.Component {
+  async componentDidMount() {
+    const apiInited = await API.init();
+
+    if (apiInited) {
+      console.log('API successfully initialized to ', apiInited);
+    } else {
+      console.error('Could not initialize API');
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Torrent searcher
+        </header>
+        <div className="App-content">
+          <Main />
+        </div>
+        <footer className="App-footer">
+          Developed by Timothée Boussus
+        </footer>
       </div>
-      <footer className="App-footer">
-        Developed by Timothée Boussus
-      </footer>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,10 +1,27 @@
 import Axios from 'axios';
 
-const axios = Axios.create({
-    baseURL: process.env.REACT_APP_API || 'http://localhost:8081',
-});
+let axios;
 
 const API = {
+    init: async () => {
+        axios = Axios.create({
+            baseURL: process.env.REACT_APP_API,
+        });
+        try {
+            await axios.get('/health');
+            return process.env.REACT_APP_API;
+        } catch (e) {
+            axios = Axios.create({
+                baseURL: process.env.REACT_APP_API_LOCAL,
+            });
+            try {
+                await axios.get('/health');
+                return process.env.REACT_APP_API_LOCAL;
+            } catch (e) {
+                return null;
+            }
+        }
+    },
     search: (search, sort, sortOrder) => axios.get(`/search/${search}`, {
         params: {
             sort, sortOrder,
