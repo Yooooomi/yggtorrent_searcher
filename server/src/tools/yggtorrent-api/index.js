d877fde2e6f895155057639f2795373a0ba63942
Timothee Boussus <timothee.boussus@gmail.com>
2020-04-11 01:38:37 +0200

    Fixed server

--- server/src/tools/yggtorrent-api/index.js
+++ server/src/tools/yggtorrent-api/index.js
@@ -3,8 +3,7 @@ var cheerio = require('cheerio');
 var jar = request.jar();
 const fs = require('fs');
 
-request.defaultParams.jar = jar;
-request.defaultParams.headers['user-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36';
+request.defaults({ jar });
 
 class Ygg {
   constructor(config) {
